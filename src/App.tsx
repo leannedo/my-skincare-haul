20f2d42765aa52a97d3a6cc344eeb09f6447a8c1
ngocdo <ngocdo0912@gmail.com>
2021-07-12 16:26:30 +0300

    refactor project to use webpack and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.less";
-import Layout from "./components/Layout/index";
+// import "./App.less";
+// import Layout from "./components/Layout/index";
 
 const App = () => {
   return (
     <div className="App">
-      <Layout />
+      <h1>Hello</h1>
     </div>
   );
 };
